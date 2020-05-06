126b8cd4dc9d7b67671af581739fb59f6910a3e2
skamranahmed <skahmed@myamu.ac.in>
2020-05-07 03:07:54 +0530

    Added comments in profit_api.py

--- profit_api.py
+++ profit_api.py
@@ -2,6 +2,7 @@ import requests
 
 URL = 'http://localhost:5000/api/profit/'
 
-response = requests.post(url = URL, json = {'r-d': 100, 'admin': 200, 'marketing': 300})
+#   making an api call and passing in the data as a json object
+response = requests.post(url = URL, json = {'res_dev': 100, 'admin': 200, 'marketing': 300})
 
 print(response.json())
