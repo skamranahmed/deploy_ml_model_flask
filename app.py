c3d11a6c4c9c1f5a9db3515eb592b268b305da07
skamranahmed <skahmed@myamu.ac.in>
2020-05-07 03:05:12 +0530

    Added comments in app.py

--- app.py
+++ app.py
@@ -1,32 +1,41 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, jsonify, request
 import pickle
 
 app = Flask(__name__)
 model = pickle.load(open('model.pickle', 'rb'))
 
 
-@app.route('/')
+@app.route('/)
 def index():
     return render_template('index.html')
 
-@app.route('/predict/', methods = ['POST'])
+
+@app.route('/predict/', methods=['POST'])
 def predict():
-    features = [float(x) for x in request.form.values()]
-    prediction = model.predict([features])
+    #   storing the values received from the form as an array in the inputs variable
+    inputs = [float(x) for x in request.form.values()]
+
+    #   predicting the values. Note that we are passing a 2D array in the model.predict method.
+    prediction = model.predict([inputs])
 
     predicted_value = round(prediction[0], 3)
-    return render_template('index.html', predicted_value = f'Profit will be: ₹{predicted_value}')
+    return render_template('index.html', predicted_value=f'Profit will be: ₹{predicted_value}')
 
 
-@app.route('/api/profit/', methods = ['POST'])
+@app.route('/api/profit/', methods=['POST'])
 def profit_api():
-    data = request.get_json(force = True)
+    #   retrieving the data got from the api call
+    data = request.get_json(force=True)
+
+    #   storing those values as a 2D array object in the inputs variable
     inputs = [list(data.values())]
+
+    #   predicting the values. Note that we are passing a 2D array in the model.predict method.
     prediction = model.predict(inputs)
+
     predicted_value = round(prediction[0], 3)
     return jsonify(predicted_value)
 
 
 if __name__ == '__main__':
-
-    app.run(debug = True)
+    app.run(debug=True)
